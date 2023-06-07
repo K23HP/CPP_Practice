d8c9bbcfa8007aad3f6edabe762c4807aad277be
K23HP <99390996+K23HP@users.noreply.github.com>
2023-06-07 10:01:15 +0800

    Update and bugfix luhn_checksum.cpp

--- side_challenges/luhn_checksum.cpp
+++ side_challenges/luhn_checksum.cpp
@@ -5,26 +5,34 @@ using namespace std;
 //Function prototype
 int double_digit(int digit);
 int add_two_digits(int digit);
+// void is_valid_input(const string user_input, bool &valid_input);
+void is_valid_checksum(int check_sum, bool &valid_checksum);
 
 int main() {
-    char digit {};
+    string digits {};
     int checksum {};
-    cout << "Enter a six digits number: ";
-    for (size_t position {0}; position < 6; ++position) {
-        cin >> digit;
-        if (position % 2 == 0) {
-            checksum += digit - '0';
+    cout << "Enter an even digit number: ";
+    cin >> digits;
+
+    // bool valid_input {false};
+
+    for (size_t index {}; index < digits.length(); ++index) {
+        if (index % 2 == 0) {
+            checksum += digits[index] - '0';
         }
         else {
-            checksum += double_digit(digit - '0');
+            checksum += double_digit(digits[index] - '0');
         }
     }
     
+    bool valid_checksum {false};
+    is_valid_checksum(checksum, valid_checksum);
+
     if (checksum % 10 == 0) {
-        cout << checksum << " is divisible by 10. Valid." << endl;
+        cout << "Checksum of " << digits << " = " << checksum << ", is divisible by 10. Valid." << endl;
     }
     else{
-        cout << checksum << " is not divisible by 10. Invalid." << endl;
+        cout << "Checksum of " << digits << " = " << checksum << ", is not divisible by 10. Invalid." << endl;
     }
 
     return 0;
@@ -35,8 +43,7 @@ int double_digit(int digit) {
 
     if (digit >= 10) {
         // split and add digits
-        digit = add_two_digits(digit);
-        return digit;
+        return add_two_digits(digit);
     }
     return digit;
 }
@@ -46,4 +53,22 @@ int add_two_digits(int digit) {
     int second_digit {digit % 10};
 
     return first_digit + second_digit;
+}
+
+// void is_valid_input(const string user_input, bool &valid_input) {
+//     if (user_input.length() % 2 == 0) {
+//         valid_input = true;
+//     }
+//     else{
+//         valid_input = false;
+//     }
+// }
+
+void is_valid_checksum(const int checksum, bool &valid_checksum) {
+    if (checksum % 10 == 0) {
+        valid_checksum = true;
+    }
+    else {
+        valid_checksum = false;
+    }
 }
